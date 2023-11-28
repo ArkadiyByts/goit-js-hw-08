a567d6c01048c006b3a808d1edbfa5ed88193cca
Arkadiy Byts <bytsarkadiy@gmail.com>
2023-11-28 18:33:22 +0100

    change video(second try)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,19 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+
+//new code
+const storedTime = localStorage.getItem('videoplayer-current-time');
+const initialTime = storedTime ? parseFloat(storedTime) : 0;
+player.setCurrentTime(initialTime);
+//new code
+
 player.on('timeupdate', throttle((ev) => {
        localStorage.setItem('videoplayer-current-time', ev.seconds); 
     }, 1000));
 
 window.addEventListener('DOMContentLoaded', () => {
     const time = localStorage.getItem('videoplayer-current-time');
+    console.log(time);
     player.setCurrentTime(time);
 });
