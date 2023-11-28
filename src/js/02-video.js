20b427dce0228ba67ab579e409a2da08f205eda6
Arkadiy Byts <bytsarkadiy@gmail.com>
2023-11-28 18:59:04 +0100

    change video(third try)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,18 +4,33 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-//new code
+// Retrieve the stored current time from local storage
 const storedTime = localStorage.getItem('videoplayer-current-time');
+
+// Set a default initial time if there's no stored time (e.g., first load)
 const initialTime = storedTime ? parseFloat(storedTime) : 0;
-player.setCurrentTime(initialTime);
-//new code
+
+// Ensure that initialTime is a valid number, default to 0 if not
+const validInitialTime = isNaN(initialTime) ? 0 : initialTime;
+
+// Set the initial time for the player
+player.setCurrentTime(validInitialTime);
 
 player.on('timeupdate', throttle((ev) => {
-       localStorage.setItem('videoplayer-current-time', ev.seconds); 
-    }, 1000));
+    // Store the current time in local storage every 1000 milliseconds (1 second)
+    localStorage.setItem('videoplayer-current-time', ev.seconds);
+}, 1000));
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+    // Retrieve the updated stored current time from local storage
     const time = localStorage.getItem('videoplayer-current-time');
-    console.log(time);
-    player.setCurrentTime(time);
+
+    // Ensure that time is a valid number, default to 0 if not
+    const validTime = isNaN(parseFloat(time)) ? 0 : parseFloat(time);
+
+    // Wait for the player to be ready before setting the current time
+    await player.ready();
+
+    // Set the current time for the player
+    player.setCurrentTime(validTime);
 });
